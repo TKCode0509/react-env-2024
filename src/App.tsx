6192c3032a489427daa622fa2639f5c4da3367f5
t.koyama@saishunkansys.com <t.koyama@saishunkansys.com>
2024-08-26 04:04:27 +0900

    mod

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React Husky aTest</h1>
+      <h1>Vite + React Husky abTest</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
